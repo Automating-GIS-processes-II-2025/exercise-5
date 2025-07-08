2da74368a7d9b6580f18b89b813903ee91676745
mikkokangasmaa <90315571+mikkokangasmaa@users.noreply.github.com>
2025-07-08 11:43:38 +0300

    Update test_exercise_5_problem_1.py

--- .tests/test_exercise_5_problem_1.py
+++ .tests/test_exercise_5_problem_1.py
@@ -2,21 +2,21 @@ from points_decorator import points
 
 class TestProblem1:
 
-    @points(0,"NOTE! The tests in this exercise simply test that you have completed the assignment, final grading is provided by the course teachers")
+    
     @points(5, "Problem 1: Did you save your map as a PNG file?")
-    def test_problem_2_plot_file_exists(self,problem2):
-        section_data, namespace = problem2
-        DATA_DIRECTORY = namespace['DATA_DIRECTORY']
+    def test_problem_1_plot_file_exists(self,problem1):
+        section_data, namespace = problem1
+        OUTPUT_DIRECTORY = namespace['OUTPUT_DIRECTORY']
 
-        png_files = list(DATA_DIRECTORY.glob("*.png"))
+        png_files = list(OUTPUT_DIRECTORY.glob("*.png"))
         assert len(png_files) > 0
 
     @points(5, "Problem 1: Did you check that the PNG file is not empty?")
-    def test_problem_2_plot_file_not_empty(self,problem2):
-        section_data, namespace = problem2
-        DATA_DIRECTORY = namespace['DATA_DIRECTORY']
+    def test_problem_1_plot_file_not_empty(self,problem1):
+        section_data, namespace = problem1
+        OUTPUT_DIRECTORY = namespace['OUTPUT_DIRECTORY']
 
-        png_files = list(DATA_DIRECTORY.glob("*.png"))
+        png_files = list(OUTPUT_DIRECTORY.glob("*.png"))
         for png_file in png_files:
             assert png_file.stat().st_size > 0
 
