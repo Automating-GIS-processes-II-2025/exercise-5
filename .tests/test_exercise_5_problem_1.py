d621c7d2032946fdf70d41d88c6c6e82120a6842
mikkokangasmaa <90315571+mikkokangasmaa@users.noreply.github.com>
2025-08-08 14:45:30 +0300

    Update test_exercise_5_problem_1.py

--- .tests/test_exercise_5_problem_1.py
+++ .tests/test_exercise_5_problem_1.py
@@ -5,18 +5,18 @@ import os
 class TestProblem1:
 
    
-    @points(5, "Problem 2: Did you save your interactive map as a HTML file?")
+    @points(5, "Problem 2: Did you save your map as a png file?")
     def test_problem_1_png_file_exists(self, problem1):
         
         png_files = glob.glob("docs/*.png", recursive=True)
         assert len(png_files) > 0
 
-    @points(5, "Problem 2: Did you check that the HTML file is not empty?")
+    @points(5, "Problem 2: Did you check that the png file is not empty?")
     def test_problem_1_png_file_not_empty(self, problem1):
         
         png_files = glob.glob("docs/*.png", recursive=True)
         
-        # Check that each HTML file has content
+        # Check that each png file has content
         for png_file in png_files:
             assert os.path.getsize(png_file) > 0
 
